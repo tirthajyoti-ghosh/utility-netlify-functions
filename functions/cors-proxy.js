6daa6e29167a940b9d20cfd3e1b95947e3aca043
Tirthajyoti Ghosh <itirthahere@gmail.com>
2023-12-31 00:23:39 +0530

    Update cors-proxy.js to handle SSL certificate verification

--- functions/cors-proxy.js
+++ functions/cors-proxy.js
@@ -1,29 +1,42 @@
-const axios = require('axios');
+const axios = require('axios').default;
+const https = require('https');
 
 exports.handler = async (event) => {
     const { httpMethod, body, headers } = event;
 
     let url = event.path.split('/.netlify/functions/cors-proxy/')[1];
     url = decodeURIComponent(url);
-    url = new URL(url);
 
-    Object.keys(event.queryStringParameters).forEach((key) => {
-        url.searchParams.append(key, event.queryStringParameters[key]);
+    // Disable SSL certificate verification (use with caution)
+    const agent = new https.Agent({
+        rejectUnauthorized: false,
     });
 
-    const response = await axios({
-        method: httpMethod,
-        url,
-        data: body,
-        headers,
-    });
+    try {
+        const response = await axios({
+            method: httpMethod,
+            url,
+            data: body,
+            headers,
+            httpsAgent: agent, // Use the agent to bypass SSL verification
+        });
 
-    return {
-        statusCode: response.status,
-        body: JSON.stringify(response.data),
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Content-Type',
-        },
-    };
+        return {
+            statusCode: response.status,
+            body: JSON.stringify(response.data),
+            headers: {
+                'Access-Control-Allow-Origin': '*', // Adjust as needed
+                'Access-Control-Allow-Headers': 'Content-Type', // Adjust as needed
+            },
+        };
+    } catch (error) {
+        return {
+            statusCode: error.response ? error.response.status : 500,
+            body: JSON.stringify({ error: 'Failed to fetch data' }),
+            headers: {
+                'Access-Control-Allow-Origin': '*', // Adjust as needed
+                'Access-Control-Allow-Headers': 'Content-Type', // Adjust as needed
+            },
+        };
+    }
 };
